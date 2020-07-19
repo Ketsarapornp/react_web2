b43a845de38f02a23f595f47fcbb009c69ee79ca
unknown <aboriginehipster@gmail.com>
2020-07-19 08:39:15 +0700

    Updates

---
+++ precache-manifest.1c23721cc1cebf153e6d0757bf2694bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e982e3117b324d8170eb5d8c35d02f40",
+    "url": "/react_web2/index.html"
+  },
+  {
+    "revision": "cd15f4c1833ed0744939",
+    "url": "/react_web2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d8c9bf2830c577b9c7a6",
+    "url": "/react_web2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd15f4c1833ed0744939",
+    "url": "/react_web2/static/js/2.da25c43b.chunk.js"
+  },
+  {
+    "revision": "33c02c42ab7314be00ddaa44dbd96c39",
+    "url": "/react_web2/static/js/2.da25c43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c9bf2830c577b9c7a6",
+    "url": "/react_web2/static/js/main.f9ed52a1.chunk.js"
+  },
+  {
+    "revision": "bbba8ad6ac8c6e7ec3ba",
+    "url": "/react_web2/static/js/runtime-main.25c0013f.js"
+  }
+]);
